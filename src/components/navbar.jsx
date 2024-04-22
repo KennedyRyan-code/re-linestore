9a28a16cd3fce83a8c177dba1262b3dec28d68be
KennedyRyan-code <kenryan49@gmail.com>
2024-04-22 13:15:31 +0300

    Updated the navbar with react icons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
+import { RiContactsLine } from "react-icons/ri";
+import { CiShop } from "react-icons/ci";
 import "./navbar.css";
 
 export const Navbar = () => {
     return (
         <div className="navbar">
             <div className="links">
-                <Link to="/">Shop</Link>
+                <Link to="/"> <CiShop size={32}/> </Link>
                 <Link to="/cart">
                     <ShoppingCart size={32} />
                 </Link>
-                <Link to="/contact">Contact Us</Link>
+                <Link to="/contact"> <RiContactsLine /> </Link>
 
             </div>
         </div>
