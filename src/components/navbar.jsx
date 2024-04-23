49d3e54f5c4948290753e195ad4f82c9fc2047c3
KennedyRyan-code <kenryan49@gmail.com>
2024-04-23 11:28:12 +0300

    Search input

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import { RiContactsLine } from "react-icons/ri";
 import { CiShop } from "react-icons/ci";
+import { FiSearch } from "react-icons/fi";
 import "./navbar.css";
 
 export const Navbar = () => {
     return (
         <div className="navbar">
+            <div  className="search"> <FiSearch /> <input type="text" className="search input" placeholder="Search" /> </div>
             <div className="links">
                 <Link to="/"> <CiShop size={32}/> </Link>
                 <Link to="/cart">
