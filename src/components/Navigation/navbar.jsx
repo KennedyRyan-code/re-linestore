1fc263534d8e43de82f9d6cbfbaef524da4b2b6a
KennedyRyan-code <kenryan49@gmail.com>
2024-04-25 16:52:42 +0300

    Moved the home link to the far left

--- src/components/Navigation/navbar.jsx
+++ src/components/Navigation/navbar.jsx
@@ -3,16 +3,21 @@ import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import { RiContactsLine } from "react-icons/ri";
 import { CiShop } from "react-icons/ci";
-import { FiSearch } from "react-icons/fi";
 import { FaBookReader } from "react-icons/fa";
 import "./navbar.css";
 
+
 export const Navbar = () => {
     return (
+    <>
         <div className="navbar">
-            <div  className="search"> <FiSearch /> <input type="text" className="search input" placeholder="Search" /> </div>
+            <div className="home-link">
+                <Link className="home-link" to="/"> <CiShop size={42}/>Shroomy Culture </Link>
+            </div>
+            <div  className="search"><input type="text" className="search input" placeholder="Search"/> </div>
+             
             <div className="links">
-                <Link to="/"> <CiShop size={32}/> </Link>
+                
                 <Link to="/cart">
                     <ShoppingCart size={32} />
                 </Link>
@@ -21,5 +26,6 @@ export const Navbar = () => {
 
             </div>
         </div>
+    </>
     )
 }
