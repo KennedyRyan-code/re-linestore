d3179935d814ad59d8bc54f3a6895d5ff1aaa6a2
KennedyRyan-code <kenryan49@gmail.com>
2024-04-26 18:51:18 +0300

    Modifications

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import "./shop.css";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
 import { Products } from "../../components/Products/Products";
+import { Recommended } from "../../components/Recommended/Recommended";
+import "./shop.css";
 
 export const Shop = () => {
     return (
+    <>
         <div className="shop">
             <div>
                 <img className="storeimg" src="https://th.bing.com/th/id/R.e11c89b1f73476246b4467f4966e4612?rik=cJ6OOsiuAXCaaQ&pid=ImgRaw&r=0" alt="Uniwear Store" />
             </div>
+            <div className="side-bar">
+                <Sidebar />
+            </div>
             <div className="shopTitle">
                 <h1>Shroomy Culture</h1>
             </div>
+            
+            <div className="Recommended">
+                <Recommended />
+            </div>
             <div className="products">
-                <Products /> <Products /> <Products />
-                
+                <Products />
             </div>
         </div>
+    </>
     )
 }
