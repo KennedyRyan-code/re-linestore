0e20526db2f9be9dbc0deae4cb505c50a70c325d
KennedyRyan-code <kenryan49@gmail.com>
2024-04-26 18:46:38 +0300

    worked on the buttons

--- src/components/Recommended/Recommended.jsx
+++ src/components/Recommended/Recommended.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import './Recommended.css'
 
 export const Recommended = () => {
   return (
-    <div>Recommended</div>
+    <div>
+      <h2 className='recommended-title'>Recommended</h2>
+      <div className='recommended-btns'>
+        <button className='btns'> All Merchandise </button>
+        <button className='btns'> Art </button>
+        <button className='btns'> T-shirts </button>
+        <button className='btns'> Pillows </button>
+        <button className='btns'> Stickers </button>
+      </div>
+    </div>
   )
 }
