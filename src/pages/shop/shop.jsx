7dc458f4baf34cb6881bdb9d01058be97244856c
KennedyRyan-code <kenryan49@gmail.com>
2024-04-27 16:35:11 +0300

    testing the outlook on shop page

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -8,7 +8,7 @@ export const Shop = () => {
     return (
     <>
         <div className="shop">
-            <div>
+            <div className="storeimg-container">
                 <img className="storeimg" src="https://th.bing.com/th/id/R.e11c89b1f73476246b4467f4966e4612?rik=cJ6OOsiuAXCaaQ&pid=ImgRaw&r=0" alt="Uniwear Store" />
             </div>
             <div className="side-bar">
@@ -16,8 +16,7 @@ export const Shop = () => {
             </div>
             <div className="shopTitle">
                 <h1>Shroomy Culture</h1>
-            </div>
-            
+            </div>          
             <div className="Recommended">
                 <Recommended />
             </div>
