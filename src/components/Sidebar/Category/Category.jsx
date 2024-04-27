9a99a2482050f5905137a72807f1a05203d81d6c
KennedyRyan-code <kenryan49@gmail.com>
2024-04-27 16:32:00 +0300

    Sidebar category component

--- src/components/Sidebar/Category/Category.jsx
+++ src/components/Sidebar/Category/Category.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import './Category.css'
+
 
 export const Category = () => {
   return (
-    <div>Category</div>
+    <div>
+      <h2 className='sidebar-title'>Category</h2>
+
+      <div>
+        <label className='sidebar-label-container'>
+          <input type='radio' name='test' />
+          <span className='checkmark'>All</span>
+        </label>
+        <label className='sidebar-lable-container'>
+          <input type='radio' name='test' />
+          <span className='checkmark'>Art</span>
+        </label>
+        <label className='sidebar-lable-container'>
+          <input type='radio' name='test' />
+          <span className='checkmark'>T-shirts</span>
+        </label>
+        <label className='sidebar-lable-container'>
+          <input type='radio' name='test' />
+          <span className='checkmark'>Pillows</span>
+        </label>
+      </div>
+    </div>
   )
 }
