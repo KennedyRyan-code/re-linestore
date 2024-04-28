d8c1b20450e5028345e11ff77c074176877d2e5c
KennedyRyan-code <kenryan49@gmail.com>
2024-04-28 11:50:19 +0300

    Modified the shop

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -8,21 +8,9 @@ export const Shop = () => {
     return (
     <>
         <div className="shop">
-            <div className="storeimg-container">
-                <img className="storeimg" src="https://th.bing.com/th/id/R.e11c89b1f73476246b4467f4966e4612?rik=cJ6OOsiuAXCaaQ&pid=ImgRaw&r=0" alt="Uniwear Store" />
-            </div>
-            <div className="side-bar">
                 <Sidebar />
-            </div>
-            <div className="shopTitle">
-                <h1>Shroomy Culture</h1>
-            </div>          
-            <div className="Recommended">
                 <Recommended />
-            </div>
-            <div className="products">
                 <Products />
-            </div>
         </div>
     </>
     )
